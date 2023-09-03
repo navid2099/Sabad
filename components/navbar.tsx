bc7fc1b87b33f0c6461b24430298fd2c53eba7ff
navid2099 <65473280+nd-github-2020@users.noreply.github.com>
2023-09-03 17:45:57 +0330

    Add settings with routes

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,5 +1,7 @@
 import { UserButton } from "@clerk/nextjs";
 
+import { MainNav } from "@/components/main-nav";
+
 const  Navbar= () => {
     return (
         <div className="border-b">
@@ -7,9 +9,7 @@ const  Navbar= () => {
                 <div>
                   This will be a store switcher  
                 </div>
-                <div>
-                    This will be routes
-                </div>
+                <MainNav className="mx-6"/>
                 <div className="ml-auto flex items-center space-x-4">
                     <UserButton afterSignOutUrl="/" />
                 </div>
