3a1a42ab29d88ba5ede713fc812ac0cfcf923a14
navid2099 <65473280+nd-github-2020@users.noreply.github.com>
2023-09-03 17:47:26 +0330

    Store switcher added with Search and Create store

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,14 +1,30 @@
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
 import { MainNav } from "@/components/main-nav";
+import StoreSwitcher from "@/components/store-switcher";
+import prismadb from "@/lib/prismadb";
 
-const  Navbar= () => {
+
+const  Navbar = async () => {
+
+    const { userId } = auth();
+
+    if(!userId){
+        redirect("/sign-in");
+    }
+
+    const stores = await prismadb.store.findMany({
+        where: {
+            userId,
+        },
+    });
+
+
     return (
         <div className="border-b">
             <div className="flex h-16 items-center px-4">
-                <div>
-                  This will be a store switcher  
-                </div>
+                  <StoreSwitcher items={stores} />
                 <MainNav className="mx-6"/>
                 <div className="ml-auto flex items-center space-x-4">
                     <UserButton afterSignOutUrl="/" />
