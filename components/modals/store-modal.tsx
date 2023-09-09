a2ee6df1923039e39603232c0eb13dbbaf7e19a4
navid2099 <65473280+nd-github-2020@users.noreply.github.com>
2023-09-09 22:05:13 +0330

    component finalize and environment settings

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -1,126 +1,84 @@
 "use client";
 
-import * as z from "zod";
-import { useForm } from "react-hook-form";
-import {zodResolver} from "@hookform/resolvers/zod"
+import * as z from "zod"
 import axios from "axios";
-import { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form";
 import { toast } from "react-hot-toast";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
-
-import { useStoreModal } from "@/hooks/use-store-modal"
-import { Modal } from "@/components/ui/modal"
-
-import { Form,
-         FormControl,
-         FormField,
-         FormItem,
-         FormLabel,
-         FormMessage 
-        } from "@/components/ui/form";
-
+import { Modal } from "@/components/ui/modal";
 import { Input } from "@/components/ui/input";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { useStoreModal } from "@/hooks/use-store-modal";
 import { Button } from "@/components/ui/button";
 
-
-
-
-
 const formSchema = z.object({
-    name: z.string().min(1),
-    });
+  name: z.string().min(1),
+});
 
 export const StoreModal = () => {
+  const storeModal = useStoreModal();
+  const router = useRouter();
 
-    const StoreModal = useStoreModal();
+  const [loading, setLoading] = useState(false);
 
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
 
-    const [loading, setloading] = useState(false)
-
-
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
-        defaultValues:{
-            name: "",            
-        },
-    });
-
-    const onSubmit = async (values: z.infer<typeof formSchema>) =>{
-
-        try{
-            setloading(true);
-
-
-            const response = await axios.post('/api/stores' , values);
-
-        //   toast.success("Store created.");
-        // for new routing
-
-
-        window.location.assign(`/${response.data.id}`);
-
-        // is going to refresh page and of course it will be shown on DB (100%)
-
-        }catch  (error) {
-            toast.error("Something went wrong.");
-        }finally {
-            setloading(false);
-        }
-        
-  
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      setLoading(true);
+      const response = await axios.post('/api/stores', values);
+      window.location.assign(`/${response.data.id}`);
+    } catch (error) {
+      toast.error('Something went wrong');
+    } finally {
+      setLoading(false);
     }
-    
-    
-    return(
-        <Modal
-        title="Create store"
-        description="Add a new Store"
-        isOpen={StoreModal.isOpen}
-        onClose={StoreModal.onClose}
-        >
+  };
 
-        <div>
-            <div className="space-y-4 py-2 pb-4">
-                <Form {...form}>
-                    <form onSubmit={form.handleSubmit(onSubmit)}>
-                        <FormField
-                            control={form.control}
-                            name="name"
-                            render={({ field }) => (
-                                <FormItem>
-                                    <FormLabel>Name</FormLabel>
-                                    <FormControl>
-                                        <Input 
-                                        disabled={loading} 
-                                        placeholder="E-Commerce" 
-                                        {...field}
-                                        />
-                                    </FormControl>
-                                    <FormMessage/>
-
-                                   
-                                </FormItem>
-
-                            )}
-
-                        />
-                    <div className="pt-6 space-x-2 flex items-center justify-end w-full">
-                        <Button 
-                            disabled= {loading}
-                            variant={"outline"} 
-                            onClick={StoreModal.onClose}>
-                             Cancel
-                        </Button>
-                        <Button disabled={loading}
-                                 type="submit">
-                                Continue
-                        </Button>
-                    </div>
-                    </form>
-                </Form>
-            </div>
+  return (
+    <Modal
+      title="Create store"
+      description="Add a new store to manage products and categories."
+      isOpen={storeModal.isOpen} 
+      onClose={storeModal.onClose}
+    >
+      <div>
+        <div className="space-y-4 py-2 pb-4">
+          <div className="space-y-2">
+            <Form {...form}>
+              <form onSubmit={form.handleSubmit(onSubmit)}>
+                <FormField
+                  control={form.control}
+                  name="name"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Name</FormLabel>
+                      <FormControl>
+                        <Input disabled={loading} placeholder="E-Commerce" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <div className="pt-6 space-x-2 flex items-center justify-end w-full">
+                  <Button disabled={loading} variant="outline" onClick={storeModal.onClose}>
+                    Cancel
+                  </Button>
+                  <Button disabled={loading} type="submit">Continue</Button>
+                </div>
+              </form>
+            </Form>
+          </div>
         </div>
-
-        </Modal>
-    );
-}
+      </div>
+    </Modal>
+  );
+};
