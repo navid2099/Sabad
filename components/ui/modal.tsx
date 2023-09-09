7aef9a5d4b57294319571aa576383c23106939c1
navid2099 <65473280+nd-github-2020@users.noreply.github.com>
2023-09-09 22:00:18 +0330

    init ui components from Shadcn into project

--- components/ui/modal.tsx
+++ components/ui/modal.tsx
@@ -1,53 +1,42 @@
-"use client"
+"use client";
 
-import { Dialog,
-     DialogContent,
-      DialogDescription,
-       DialogHeader,
-        DialogTitle 
-} from "@/components/ui/dialog";
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
 
-
-interface ModalProps{
-
-    title: string;
-    description: string;
-    isOpen: boolean;
-    onClose: () => void;
-    children?: React.ReactNode;
+interface ModalProps {
+  title: string;
+  description: string;
+  isOpen: boolean;
+  onClose: () => void;
+  children?: React.ReactNode;
 }
-
 
 export const Modal: React.FC<ModalProps> = ({
-    title,
-    description,
-    isOpen,
-    onClose,
-    children
-}) =>{
-    const onChange =(open: boolean) =>{
-        if(!open){
-            onClose();
-        }
-    };
+  title,
+  description,
+  isOpen,
+  onClose,
+  children
+}) => {
+  const onChange = (open: boolean) => {
+    if (!open) {
+      onClose();
+    }
+  };
 
-
-    return(
-        <Dialog open={isOpen} onOpenChange={onChange}>
-            <DialogContent>
-                <DialogHeader>
-                    <DialogTitle> {title} </DialogTitle>
-
-                    <DialogDescription>
-                        {description}
-                    </DialogDescription>
-                </DialogHeader>
-                <div>
-                    {children}
-                </div>
-            </DialogContent>
-
-        </Dialog>
-    )
-
-}
+  return ( 
+    <Dialog open={isOpen} onOpenChange={onChange}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>{title}</DialogTitle>
+          <DialogDescription>
+            {description}
+          </DialogDescription>
+        </DialogHeader>
+        <div>
+          {children}
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+};
